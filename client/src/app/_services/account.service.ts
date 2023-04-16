951b1a1a2fd6007261bf81c13fe8fe48fa403cbb
JeanCRECHRIOU <jchev34@gmail.com>
2023-04-16 18:13:43 +0200

    End of section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -29,7 +29,7 @@ export class AccountService {
 
         if (user) {
           localStorage.setItem('user', JSON.stringify(user))
-          this.currentUserSource.next(user);
+          this.setCurrentUser(user);
           console.log("this.currentUser$");
           console.log(this.currentUser$);
         }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { FormsModule } from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent, MemberListComponent, MemberDetailComponent, ListsComponent, MessagesComponent],
@@ -24,13 +22,7 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    TooltipModule.forRoot(),
-    BsDropdownModule.forRoot(),
-    ToastrModule.forRoot(
-      {
-        positionClass:'toast-bottom-right'
-      }
-    )
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent],
